beef634f5bbbcbc102403c096e1d64eb97022918
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-12 10:34:20 -0300

    feat: add custom title text component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -16,4 +16,13 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 )
 
-export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial='hidden'
+    whileInView='show'
+    className={`mt-2 font-bold md:text-6xl text-4xl text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
+)
